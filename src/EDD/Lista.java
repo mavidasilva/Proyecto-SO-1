05a01e249ef10312acf85135bec3e9e359ddc740
mavidasilva <122273782+mavidasilva@users.noreply.github.com>
2025-10-16 15:34:42 -0400

    Lista completa :sparkles:

--- src/EDD/Lista.java
+++ src/EDD/Lista.java
@@ -4,15 +4,16 @@
  */
 package EDD;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author mariavictoriadasilvanunez
  */
 public class Lista {
-     private Nodo pFirst;
+    private Nodo pFirst;
     private int size;
     
-    //Constructor de la clase Lista
     public Lista() {
         this.pFirst = null;
         this.size = 0; 
@@ -35,7 +36,267 @@ public class Lista {
     }
     
     
-    public boolean EsVacio(){
+    public boolean isEmpty(){
         return this.pFirst == null;
+    }
+    
+    public void vaciar(){
+        this.pFirst = null;
+        this.size = 0;   
+    }
+    
+    public Nodo insertarInicio(Object dato){
+        
+        Nodo pNew = new Nodo();
+        pNew.setDato(dato);
+       
+        if (isEmpty()) {   
+            pFirst = pNew;
+        } else{
+            pNew.setPnext(pFirst);
+            pFirst = pNew;
+        }
+        size++;
+        return pNew;
+    }
+    
+
+    public void InsertarFinal(Object dato){
+        Nodo pNew = new Nodo(dato);
+        if(isEmpty()){
+            pFirst = pNew;
+        }else{
+            Nodo aux = pFirst;
+            while (aux.getPnext() != null){
+                aux = aux.getPnext();
+            }
+            aux.setPnext(pNew);
+        }
+        size++;
+    }
+    
+    public void insertarPorPosicion(int posicion, Object valor){
+        if(posicion>=0 && posicion<size){
+            Nodo nuevo = new Nodo(valor);
+            if(posicion == 0){
+                nuevo.setPnext(pFirst);
+                pFirst = nuevo;
+            }
+            else{
+                if(posicion == size-1){
+                    Nodo aux = pFirst;
+                    while(aux.getPnext() != null){
+                        aux = aux.getPnext();
+                    }
+                    aux.setPnext(nuevo);              
+                }
+                else{
+                    Nodo aux = pFirst;
+                    for (int i = 0; i < (posicion-1); i++) {
+                        aux = aux.getPnext();
+                    }
+                    Nodo siguiente = aux.getPnext();
+                    aux.setPnext(nuevo);
+                    nuevo.setPnext(siguiente);
+                }
+            }
+            size++;
+        }
+    }
+
+    public void insertarPorReferencia(Object ref,Object valor){
+        
+        Nodo nuevo = new Nodo();
+        nuevo.setDato(valor);
+        
+        if (!isEmpty()) {
+            if (buscar(ref)) {
+                Nodo aux = pFirst;
+                while (aux.getDato() != ref) {
+                    aux = aux.getPnext();
+                }
+
+                Nodo siguiente = aux.getPnext();
+                aux.setPnext(nuevo);
+                nuevo.setPnext(siguiente);
+            
+                size++;
+            }
+        }
+    }
+
+    public String transformar(){
+        if(!isEmpty()){
+            
+            Nodo aux = pFirst;
+            String expresion = "";
+            
+            for(int i = 0; i <size;i++){
+            expresion += aux.getDato().toString() + "\n";
+            aux = aux.getPnext();
+            }
+            return expresion;
+        }
+        return "Lista vacia";
+    }
+    
+    public void mostrar(){
+        if (!isEmpty()){
+            Nodo aux = pFirst;
+            String expresion = "";
+            while(aux != null){
+               expresion = expresion + aux.getDato().toString() + "\n";
+               aux = aux.getPnext();
+            }
+            JOptionPane.showMessageDialog(null,expresion);
+            
+        }else{
+            JOptionPane.showMessageDialog(null, "La lista esta vacia");
+        }
+    }
+    
+    public boolean eliminarInicio(){
+        if(!isEmpty()){
+            pFirst = pFirst.getPnext();
+            size--;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public void eliminarFinal(){
+        if(!isEmpty()){
+            if (getSize()==1) {
+                vaciar();
+            }else{
+                Nodo pointer = getpFirst();
+                while(pointer.getPnext() != null && pointer.getPnext().getPnext()!= null){
+                    pointer = pointer.getPnext();
+                }
+                pointer.setPnext(null);
+            }
+            size --;
+        }  
+    }
+    
+    public void eliminarPorReferencia(Object referencia){
+
+        if (buscar(referencia)) {
+            if (pFirst.getDato() == referencia) {
+                pFirst = pFirst.getPnext();
+            } else{
+                Nodo aux = pFirst;
+                while(aux.getPnext().getDato() != referencia){
+                    aux = aux.getPnext();
+                }
+                Nodo siguiente = aux.getPnext().getPnext();
+                aux.setPnext(siguiente);  
+            }
+            size--;
+        }
+    }
+    
+    public void eliminarPorPosicion(int posicion){
+
+        if(posicion>=0 && posicion<size){
+            if(posicion == 0){
+                pFirst = pFirst.getPnext();
+            }
+            else{
+                Nodo aux = pFirst;
+                for (int i = 0; i < posicion-1; i++) {
+                    aux = aux.getPnext();
+                }
+                Nodo siguiente = aux.getPnext();
+                aux.setPnext(siguiente.getPnext());
+            }
+            size--;
+        }
+    }
+    
+    public void editarPorReferencia(Object referencia, Object dato){
+        if (buscar(referencia)) {
+            Nodo aux = pFirst;
+            while(aux.getDato() != referencia){
+                aux = aux.getPnext();
+            }
+            aux.setDato(dato);
+        }
+    }
+    
+    //Metodo para editar el valor de un nodo en una posicion
+    public void editarPorPosicion(int posicion , Object dato){
+
+        if(posicion>=0 && posicion<size){
+            if(posicion == 0){
+                pFirst.setDato(dato);
+            }
+            else{
+                Nodo aux = pFirst;
+
+                for (int i = 0; i < posicion; i++) {
+                    aux = aux.getPnext();
+                }
+                aux.setDato(dato);
+            }
+        }
+    }
+    
+    //Metodo para obtener el valor de un nodo en una determinada posición
+    public Object getValor(int posicion){
+
+        if(posicion>=0 && posicion<size){
+            
+            if (posicion == 0) {
+                return pFirst.getDato();
+            }else{
+                Nodo aux = pFirst;
+                for (int i = 0; i < posicion; i++) {
+                    aux = aux.getPnext();
+                }
+                return aux.getDato();
+            }
+        }
+        return null;
+    }
+    
+    
+    //Consulta la posición de un elemento en la lista
+    public int getPosicion(Object referencia){
+
+        if (buscar(referencia)) {
+            
+            Nodo aux = pFirst;
+            int cont = 0;
+            while(referencia != aux.getDato()){
+                cont ++;
+                aux = aux.getPnext();
+            }
+            return cont;
+        } else {
+            return -1;
+        }
+    }
+    
+    // Funcion para buscar un elemento en la lista
+    public boolean buscar(Object referencia){
+        Nodo aux = pFirst;
+        boolean encontrado = false;
+        while(aux != null && encontrado != true){
+            if (referencia == aux.getDato()){ 
+                encontrado = true;
+            }
+            else{
+                aux = aux.getPnext();
+            }
+        }
+        return encontrado;
+    }
+    
+    //Destructor
+    public void eliminar(){
+        pFirst = null;
+        size = 0;
     }
 }
