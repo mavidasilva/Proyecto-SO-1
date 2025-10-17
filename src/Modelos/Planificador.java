96170ffbac7a7c9877e519533d46bf03a6e4e6a3
mavidasilva <122273782+mavidasilva@users.noreply.github.com>
2025-10-17 11:54:18 -0400

    inicializar modelos identificados :construction:

---
+++ src/Modelos/Planificador.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author mariavictoriadasilvanunez
+ */
+public class Planificador {
+   
+    
+}
